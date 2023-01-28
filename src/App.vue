c87529aaf2310e2171284ddb93858a86dd0f371a
sunpm <15022352@qq.com>
2023-01-28 17:29:35 +0800

    添加 naiveui 并按需自动引入

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+const {message} = createDiscreteApi(['message'])
+const showMsg = () => {
+  message.info('Hello World')
+}
 </script>
 
 <template>
   <div class="border p-4">
-    测试 windicss
+    测试  windicss
   </div>
+
+  <n-button @click="showMsg">naive-ui</n-button>
 </template>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,30 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import {NaiveUiResolver} from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     WindiCSS(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'naive-ui': [
+            'useDialog',
+            'createDiscreteApi',
+            'useNotification',
+            'useLoadingBar'
+          ]
+        }
+      ]
+    }),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
   ],
 })
